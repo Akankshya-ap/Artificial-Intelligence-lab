cca8a88a9a1d3a42104a5877f96d566c20075129
Akankshya Patra <ayesha.patra2@gmail.com>
2018-08-29 11:46:25 +0530

    Add files via upload

---
+++ slp1.py
@@ -0,0 +1,52 @@
+from pylab import plot, ylim
+from random import choice
+from numpy import array, dot, random
+#from pylab import plot, ylim
+import numpy as np
+import matplotlib.pyplot as plt
+
+unit_step = lambda x: 0 if x < 0 else 1
+#print()
+
+training_data = [
+    (array([0,0,1]), 0),
+    (array([0,1,1]), 1),
+    (array([1,0,1]), 1),
+    (array([1,1,1]), 1),
+]
+
+w = random.rand(3)
+j=np.array([
+    [0,0,1],
+    [0,1,1],
+    [1,0,1],
+    [1,1,1],
+])
+errors = []
+eta = 0.2
+n = 100
+for i in range(n):
+    x, expected = choice(training_data)
+    result = dot(w, x)
+    error = expected - unit_step(result)
+    errors.append(error)
+    w += eta * error * x
+  
+#fig,ax = plt.subplots()
+for x, _ in training_data:
+    result = dot(x, w)
+    print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+    
+    #print(x[0],(-w[0]*x[0])/w[1])
+    #ax.plot(x[:1], ((-w[:1]*x[:1])/w[2])
+for d, sample in enumerate(j):
+    # Plot the negative samples
+    if d < 1:
+        plt.scatter(sample[0], sample[1], s=120, marker='_', linewidths=2)
+    # Plot the positive samples
+    else:
+        plt.scatter(sample[0], sample[1], s=120, marker='+', linewidths=2)
+
+plt.plot([0,0.9],[0.95,0])
+#ylim([-1,1])
+#plot(errors)
